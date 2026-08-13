536b5ba46a923a304b809a242af338b3eed984be
Ebenezer Akhalumenyo <ebenezer.i.akhalumenyo@gmail.com>
2026-08-13 23:42:05 +0100

    Initialize Tailwind CSS configuration
    Add Tailwind CSS configuration with custom colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,16 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#09090C",
+      },
+    },
+  },
+  plugins: [],
+};
